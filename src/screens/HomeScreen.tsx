b7e9fd46c17d0ed9642503ecda6110ff1dc42897
juliflorezg <forezjuli08@hotmail.com>
2022-12-29 16:31:23 -0500

    add pokeball image and title

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,25 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {View, Text, Button} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import {View, Text, Button, Image} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+// import Icon from 'react-native-vector-icons/FontAwesome5';
+import {styles} from '../theme/appTheme';
 
 interface Props extends StackScreenProps<any, any> {}
 
 export const HomeScreen = ({navigation}: Props) => {
+  const {top} = useSafeAreaInsets();
+
   return (
     <View>
-      <Icon name="dot-circle" size={30} color="red" />
-      <Text>home</Text>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.pokebolaBG}
+      />
+      {/* <Icon name="dot-circle" size={30} color="red" /> */}
+      <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
+        Pokedex
+      </Text>
       <Button
         title="Go to Pokemon page"
         onPress={() => navigation.navigate('PokemonScreen')}

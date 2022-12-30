0d7d1a06bc557d8313e6b4894e732ea638a63e23
juliflorezg <forezjuli08@hotmail.com>
2022-12-29 19:16:40 -0500

    use the custom hook for getting pokemons and displaying them in home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Text, Button, Image} from 'react-native';
+import {Text, Button, Image, ActivityIndicator} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
 // import Icon from 'react-native-vector-icons/FontAwesome5';
@@ -10,7 +11,7 @@ interface Props extends StackScreenProps<any, any> {}
 
 export const HomeScreen = ({navigation}: Props) => {
   const {top} = useSafeAreaInsets();
-  usePokemonPaginated();
+  const {isLoading, singlePokemonList, loadPokemons} = usePokemonPaginated();
 
   return (
     <>
@@ -22,6 +23,24 @@ export const HomeScreen = ({navigation}: Props) => {
       <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
         Pokedex
       </Text>
+
+      <FlatList
+        data={singlePokemonList}
+        renderItem={({item}) => (
+          <Image
+            source={{uri: item.picture}}
+            style={{width: 100, height: 100}}
+          />
+        )}
+        keyExtractor={pokemon => pokemon.id}
+        //infinite scroll
+        onEndReached={loadPokemons}
+        onEndReachedThreshold={0.4}
+        ListFooterComponent={
+          <ActivityIndicator style={{height: 100}} size={30} color="black" />
+        }
+        showsVerticalScrollIndicator={false}
+      />
       <Button
         title="Go to Pokemon page"
         onPress={() => navigation.navigate('PokemonScreen')}
